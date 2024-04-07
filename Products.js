332f3407e35550a53fba74cfb12ce769f1ea1af8
SamRowland <114190072+SamRowland@users.noreply.github.com>
2024-04-06 19:40:16 -0500

    Add files via upload

---
+++ Products.js
@@ -0,0 +1,38 @@
+export const Products = [
+    {
+        "id": "1",
+        "productName": "Cinnamon Roll",
+        "productImage": "./pastry/cinnamon rolls.jpg",
+        "price" : "4.00"
+    },
+    {
+        "id": "2",
+        "productName": "Croissant",
+        "productImage": "./pastry/croissant.jpg",
+        "price" : "3.00" 
+    },
+    {
+        "id": "3",
+        "productName": "Danish Pastry",
+        "productImage": "./pastry/danish pastry.jpg",
+        "price" : "3.50" 
+    },
+    {
+        "id": "4",
+        "productName": "Eclair Pastry",
+        "productImage": "./pastry/eclair pastry.jpg",
+        "price" : "3.50" 
+    },
+    {
+        "id": "5",
+        "productName": "Puff Pastry",
+        "productImage": "./pastry/puff pastry.jpg",
+        "price" : "3.00" 
+    },
+    {
+        "id": "6",
+        "productName": "Sourdough",
+        "productImage": "./bread/sourdough.jpg",
+        "price" : "4.00" 
+    },
+]
